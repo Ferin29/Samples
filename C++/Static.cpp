259f4f2b71dd194d76e2556fc1ba83ac8aa068a0
navarr_a <navarr_a@epitech.eu>
2015-01-20 11:31:28 +0100

    Push

--- C++/Static.cpp
+++ C++/Static.cpp
@@ -1,1 +1,8 @@
+// -------- Static Cast --------
 var = static_cast<dest type>(var source)
+/**/
+class Fruit{};
+class Banana: public Fruit{};
+Banana banana;
+Fruit& fruitRef = banana;
+Banana& bananaRef = static_cast<Banana&>(fruitRef);
